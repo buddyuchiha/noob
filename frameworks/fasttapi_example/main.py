3a25bfe73db14ea03c3ac64e95e886c63fcccd9c
Buddy <Pro100biv@mail.ru>
2025-05-02 20:11:30 +0400

    begin fastapi learn

---
+++ frameworks/fasttapi_example/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional 
+
+app = FastAPI()
+
+class Task(BaseModel):
+    name: str 
+    description: Optional[str] = None 
+
+@app.get("/home")
+def get_tasks():
+    return {"data" : "дом"}
+
+@app.get("/tasks")
+def get_tasks():
+    task = Task(name="Запиши это видео")
+    return {"data" : task}
+
